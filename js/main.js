c8f2be1b7c9b0f3b850982385ae1de93af294a20
Yode <yode0419@gmail.com>
2023-07-12 01:43:42 +0800

    增加EmuJr !!!

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+alert("test");
 let myImage = document.querySelector('#pekoImage');
 
 myImage.onclick = function () {
@@ -9,28 +10,127 @@ myImage.onclick = function () {
     }
 }
 
-//
-//let myButton = document.querySelector('button');
-//let myHeading = document.querySelector('h2');
-//  
-//function setUserName() {
-//    let myName = prompt('請輸入您ㄉ暱稱');
-//    if (!myName) {
-//        setUserName();
-//    } else {
-//        localStorage.setItem('name', myName);
-//        myHeading.textContent = '尼好! ' + myName + ' 歡迎光臨 ~ ~ (つ´ω`)つ';
-//    }
-//}
-//
-//if (!localStorage.getItem('name')) {
-//    setUserName();
-//} else {
-//    let storedName = localStorage.getItem('name');
-//    myHeading.textContent = '尼好! ' + storedName + ' 歡迎光臨 ~ ~ (つ´ω`)つ';
-//}
-//
-//myButton.onclick = function () {
-//    setUserName();
-//    localStorage.clear();
-//}
+
+import * as THREE from 'three';
+import {OrbitControls} from 'three/addons/controls/OrbitControls.js';
+import {GLTFLoader} from 'three/addons/loaders/GLTFLoader.js';
+
+
+let container = document.querySelector('.container3D');
+let emu, mixer, emuRun, clock = new THREE.Clock();
+// 產生一個場景
+const scene = new THREE.Scene();
+
+// 產生一個相機
+const camera = new THREE.PerspectiveCamera(
+    75,
+    container.clientWidth / container.clientHeight,
+    0.1,
+    1000
+);
+
+// 設定相機的位置
+camera.position.set(2, 4, 5);
+
+// 選定渲染器
+const renderer = new THREE.WebGLRenderer({
+    alpha: true
+});
+//{antialias: true, alpha: true}
+// 初始渲染畫面尺寸
+renderer.setSize(container.clientWidth, container.clientHeight);
+
+// 加入 canvas 元素供渲染畫面
+container.appendChild(renderer.domElement);
+
+// 設定軌道控制器，讓相機可以環繞觀察場景
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.update();
+
+// 產生平面物體
+const planeGeometry = new THREE.PlaneGeometry(6, 6);
+const planeMaterial = new THREE.MeshBasicMaterial({
+    color: '#6D6D6D',
+    // 雙面著色
+    side: THREE.DoubleSide,
+});
+const plane = new THREE.Mesh(planeGeometry, planeMaterial);
+plane.rotation.x = Math.PI / 2;
+
+// 設定平面物體在場景的位置
+plane.position.set(0, 0, 0);
+scene.add(plane);
+
+// 產生一個藍色正方形物體
+const cubeGeometry = new THREE.BoxGeometry(1, 1, 1);
+const cubeMaterial = new THREE.MeshLambertMaterial({
+    color: '#429ef5'
+});
+
+const cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
+
+// 設定正方形物體的位置
+cube.position.set(0, 0, 5);
+scene.add(cube);
+
+// Load Modal
+let loader = new GLTFLoader();
+loader.load('./public/EmuJr.gltf',
+    function (gltf) {
+        //If the file is loaded, add it to the scene
+        emu = gltf.scene;
+        scene.add(emu);
+        
+    
+        let fileAnimations = gltf.animations;
+        mixer = new THREE.AnimationMixer(emu);
+        let animationName = THREE.AnimationClip.findByName(fileAnimations, 'ArmatureAction')
+        emuRun = mixer.clipAction(animationName);
+        emuRun.play();
+    
+    },
+    function (xhr) {
+        //While it is loading, log the progress
+        console.log((xhr.loaded / xhr.total * 100) + '% loaded');
+    },
+    function (error) {
+        //If there is an error, log it
+        console.error(error);
+    }
+);
+
+// 建立光源
+let pointLight = new THREE.PointLight(0xffffff, 1);
+pointLight.position.set(5, 10, 10);
+scene.add(pointLight);
+
+const ambientLight = new THREE.AmbientLight(0x333333, 2);
+scene.add(ambientLight);
+
+// 設定動畫
+function animate() {
+    // 循環觸發渲染以產生動畫
+    requestAnimationFrame(animate);
+
+    if (mixer) {
+        mixer.update(clock.getDelta());
+    }
+    // 設定正方形轉動效果
+    //
+    //    cube.rotation.y += 0.01;
+
+    renderer.render(scene, camera);
+}
+
+// 開始執行動畫
+animate();
+
+// 配合視窗大小自動更新
+function onWindowResize(){
+    camera.aspect = container.clientWidth/container.clientHeight;
+    camera.updateProjectionMatrix();
+    
+    renderer.setSize(container.clientWidth, container.clientHeight);
+}
+
+window.addEventListener('resize',onWindowResize);
