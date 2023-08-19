f935fcda9601fcff3ca27612381d7ae32eb9721c
Yode <yode0419@gmail.com>
2023-08-19 14:59:47 +0800

    小調整
    >網頁favicion
    >背景淡入開場
    >navbar底色小修
    >css-doodle稍微鮮豔一點
    >卡片增加邊框、hover時有閃光效果
    >arrowDown動畫修改

--- js/lottieIcon.js
+++ js/lottieIcon.js
@@ -1,33 +1,41 @@
+//歡迎頁面arrowDown動畫
 const arrowDownElements = document.querySelectorAll('.arrowDown');
-
-arrowDownElements.forEach(arrowDown => {
-    const arrowDownAnimtion = lottie.loadAnimation({
-        container: arrowDown,
-        renderer: 'svg',
-        loop: true,
-        autoplay: false,
-        path: './js/arrowDown.json'
-    });
-
-    // 鼠标移到上面时播放动画
-    arrowDown.addEventListener('mouseenter', () => {
-        arrowDownAnimtion.play();
-    });
-
-    // 鼠标移出时取消动画
-    arrowDown.addEventListener('mouseleave', () => {
-        arrowDownAnimtion.goToAndStop(0, false);
-    });
+lottie.loadAnimation({
+    container: arrowDown,
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: './js/arrowDown.json'
 });
 
 
+//arrowDownElements.forEach(arrowDown => {
+//    const arrowDownAnimtion = lottie.loadAnimation({
+//        container: arrowDown,
+//        renderer: 'svg',
+//        loop: true,
+//        autoplay: false,
+//        path: './js/arrowDown.json'
+//    });
+//
+//    // 鼠标移到上面时播放动画
+//    arrowDown.addEventListener('mouseenter', () => {
+//        arrowDownAnimtion.play();
+//    });
+//
+//    // 鼠标移出时取消动画
+//    arrowDown.addEventListener('mouseleave', () => {
+//        arrowDownAnimtion.goToAndStop(0, false);
+//    });
+//});
 
 
 
 
 
+
+//Footer的logo動畫
 const logo = document.querySelector('.logo');
-
 lottie.loadAnimation({
     container: logo,
     renderer: 'svg',
@@ -40,6 +48,8 @@ lottie.loadAnimation({
 
 
 
+
+//留言送出噴彩帶動畫
 const submitBtn = document.getElementById('submitBtn');
 const confetti = document.getElementById('confetti'); // 將其添加到適當的容器中
 const form = document.getElementById('googleForm'); // 取得表單元素
@@ -59,7 +69,7 @@ const animation = lottie.loadAnimation(animationConfig);
 // 按鈕點擊事件處理
 form.addEventListener('submit', (event) => {
     event.preventDefault();
-    
+
     if (form.checkValidity()) {
         confetti.style.zIndex = '1';
 
